a1ccf6cfc1afb8981011d51778a2bd722eec1dc6
y-temp4 <y.temp4@gmail.com>
2019-08-10 21:46:56 +0900

    Add icon file

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
     ],
     link: [
       { rel: 'manifest', href: '/manifest.json' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.jpg' }
     ]
   },
   /*
