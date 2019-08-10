52a756df8e42af1df77e49dfefa896e950431e01
y-temp4 <y.temp4@gmail.com>
2019-08-10 23:41:32 +0900

    Add contact form

---
+++ pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          お名前:
+          <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          メールアドレス:
+          <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          本文:
+          <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">送信</button>
+      </p>
+    </form>
+  </main>
+</template>
