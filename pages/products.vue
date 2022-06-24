92d1efe06d6bcbb3ecd98488747e9ee1a5caeb71
y-temp4 <y.temp4@gmail.com>
2022-06-24 12:32:55 +0900

    Update to show that the product is out in suspension

--- pages/products.vue
+++ pages/products.vue
@@ -2,20 +2,28 @@
   <main>
     <MainHeading>Products</MainHeading>
     <p>これまでに作ったサービスの一覧です。</p>
-    <div v-for="product in products" :key="product.name">
+    <label class="show-suspended-products-label">
+      <input type="checkbox" v-model="showSuspendedProducts" />
+      停止中のサービスを表示する
+    </label>
+    <div v-for="product in filteredProducts" :key="product.name">
       <h3 class="product-name">
-        <a
-          :href="product.product_url"
-          target="_blank"
-          rel="noopener noreferrer"
+        <component
+          :is="!product.suspended ? 'a' : 'span'"
+          :href="!product.suspended ? product.productURL : null"
+          :target="!product.suspended ? '_blank' : null"
+          :rel="!product.suspended ? 'noopener noreferrer' : null"
         >
           {{ product.name }}
-        </a>
+        </component>
+        <span v-if="product.suspended" class="product-suspended-label"
+          >停止中</span
+        >
       </h3>
       <p>
         {{ product.description }} 👉
         <a
-          :href="product.introduction_url"
+          :href="product.introductionURL"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -28,15 +36,16 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { MetaInfo } from 'vue-meta'
+import type { MetaInfo } from 'vue-meta'
 import MainHeading from '~/components/MainHeading.vue'
-import products from '~/data/products.json'
+import { products } from '~/data/products'
+import type { Product } from '~/data/products'
 
 export default Vue.extend({
   components: { MainHeading },
   data() {
     return {
-      products
+      showSuspendedProducts: false
     }
   },
   head(): MetaInfo {
@@ -45,12 +54,28 @@ export default Vue.extend({
       title,
       meta: [{ hid: 'og:title', property: 'og:title', content: title }]
     }
+  },
+  computed: {
+    filteredProducts(): Product[] {
+      if (this.showSuspendedProducts) {
+        return products
+      }
+      return products.filter(p => !p.suspended)
+    }
   }
 })
 </script>
 
 <style lang="postcss" scoped>
+.show-suspended-products-label {
+  @apply select-none cursor-pointer text-sm flex items-center gap-2 py-1;
+}
+
 .product-name {
-  @apply text-xl text-left w-full font-bold mt-5 mb-1;
+  @apply text-xl text-left w-full font-bold mt-5 mb-1 flex items-center gap-2;
+}
+
+.product-suspended-label {
+  @apply text-xs bg-yellow-5 p-1 rounded text-gray-9;
 }
 </style>
