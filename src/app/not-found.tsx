4a81dfdb97e3750e12db366cb9e1d3fb1e881cb3
y-temp4 <y.temp4@gmail.com>
2024-05-31 09:46:52 +0900

    Fix lint

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="mx-auto max-w-screen-xl py-20">
       <div className="mx-auto max-w-screen-sm text-center">
-        <h1 className="text-8xl font-bold mb-5">404</h1>
+        <h1 className="mb-5 text-8xl font-bold">404</h1>
         <p>ページが見つかりませんでした。</p>
         <Link href="/">トップに戻る</Link>
       </div>
