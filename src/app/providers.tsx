cc5dbeef1cce26fc28dc28ff3e8420af66a57813
y-temp4 <13657589+y-temp4@users.noreply.github.com>
2024-10-31 23:13:14 +0900

    Update nuqs

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from 'next-themes';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import type { JSX, PropsWithChildren } from 'react';
 
 export const Providers = ({ children }: PropsWithChildren): JSX.Element => (
-  <ThemeProvider storageKey="theme-y-temp4.com">{children}</ThemeProvider>
+  <ThemeProvider storageKey="theme-y-temp4.com">
+    <NuqsAdapter>{children}</NuqsAdapter>
+  </ThemeProvider>
 );
