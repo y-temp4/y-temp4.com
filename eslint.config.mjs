cfda697144b5f8d914bfa11f757ebdcf86ff3b0b
y-temp4 <13657589+y-temp4@users.noreply.github.com>
2024-11-01 00:03:11 +0900

    Use typescript-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,15 @@
+// @ts-check
+
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import importPlugin from 'eslint-plugin-import';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tailwind from 'eslint-plugin-tailwindcss';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,16 +20,19 @@ const compat = new FlatCompat({
 });
 
 const config = [
-  ...tailwind.configs['flat/recommended'],
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...tailwind.configs['flat/recommended'],
+  ...tseslint.config({
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  }),
   {
     plugins: {
-      '@typescript-eslint': typescriptEslint,
       import: importPlugin,
       'simple-import-sort': simpleImportSort,
     },
     rules: {
-      '@typescript-eslint/consistent-type-imports': 'error',
       'import/first': 'error',
       'import/newline-after-import': 'error',
       'import/no-duplicates': 'error',
