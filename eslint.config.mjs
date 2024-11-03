39c4e3dce82ca87abbd280e28fcf7cb5b34cdab1
y-temp4 <13657589+y-temp4@users.noreply.github.com>
2024-11-03 10:18:45 +0900

    Remove unused ESLint dependencies and simplify configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,13 @@
 // @ts-check
 
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import importPlugin from 'eslint-plugin-import';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tailwind from 'eslint-plugin-tailwindcss';
 import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+const compat = new FlatCompat();
 
 const config = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
