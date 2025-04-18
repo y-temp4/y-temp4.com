c2c97087c79e21a4c21889844d458637602427a8
y-temp4 <13657589+y-temp4@users.noreply.github.com>
2025-04-18 11:28:00 +0900

    Update packages

--- src/app/(root)/(introduction)/about/page.tsx
+++ src/app/(root)/(introduction)/about/page.tsx
@@ -103,7 +103,7 @@ export default function Page() {
       </p>
       <h3 className={heading}>ID について</h3>
       <p> 普段は以下の ID で活動していることが多いです。 </p>
-      <ul className="ml-6 mb-4 list-disc">
+      <ul className="mb-4 ml-6 list-disc">
         <li>y_temp4</li>
         <li>y-temp4</li>
         <li>yt4</li>
